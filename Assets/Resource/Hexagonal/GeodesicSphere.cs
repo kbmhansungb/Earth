b95e383763322c381a63a857d891c691d96e976a
kbmhansungb <b.m.kim@weroadin.com>
2023-03-17 23:20:29 +0900

    런타임에 메쉬를 만들고 렌더되도록 만듭니다.

--- Assets/Resource/Hexagonal/GeodesicSphere.cs
+++ Assets/Resource/Hexagonal/GeodesicSphere.cs
@@ -8,20 +8,47 @@ public class GeodesicSphere : MonoBehaviour
     {
         Mesh mesh = new Mesh();
 
-        // generate vertices
-        Vector3[] vertices = new[] {
-            // generating three vertices for triangle
-            new Vector3(0.0f, 0.0f, 0.0f),
-            new Vector3(-1.0f, 1.0f, 0.0f),
-            new Vector3(1.0f, 1.0f, 0.0f),
-        };
-        Vector2[] uv = new[] {
-    // generated vertices will be mapped to corresponding coordinates in uv
-    new Vector2(0.5f, 0.0f),
-    new Vector2(0.0f, 1.0f),
-    new Vector2(1.0f, 1.0f),
-};
-        mesh.uv = uv;
-        mesh.vertices = vertices;
+        Vector3[] vertices = new [] {
+             // creating vertices of quad. aligning them in shape of square
+             
+             new Vector3(0, 0, 0),
+             new Vector3(0, 1, 0),
+             new Vector3(1, 0, 0),
+             new Vector3(1, 1, 0),
+         };
+         mesh.vertices = vertices;
+         
+         
+         // generate uv
+         Vector2[] uv = new [] {
+             // generate uv for corresponding vertices also in form of square
+             
+             new Vector2(0, 0),
+             new Vector2(0, 1),
+             new Vector2(1, 0),
+             new Vector2(1, 1),
+         };
+         mesh.uv = uv;
+         
+         Vector3[] normals = new [] {
+             // normals same as tris
+             -Vector3.forward,
+             -Vector3.forward,
+             -Vector3.forward,
+             -Vector3.forward,
+         };
+         mesh.normals = normals;
+         
+         int[] triangles = new [] {
+             // tris are viewed as group of three
+             // remember to order them in clockwise
+             // position of index is not importaint as long as they are in clockwise order
+             
+             0,1,2,// first tris
+             2,1,3// second tris
+         };
+         mesh.triangles = triangles;
+
+         meshFilter.mesh = mesh;
     }
 }
