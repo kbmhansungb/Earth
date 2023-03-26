9ce7ca621a8363f7e093c6ccff8b02500ca1e688
kbmhansungb <kbmhansungb@gmail.com>
2023-03-26 15:30:34 +0900

    HexagonalTest로 이름 변경

--- Assets/Resource/HexagonalTest/HexagonalTest.cs
+++ Assets/Resource/HexagonalTest/HexagonalTest.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 namespace BM.MeshGenerator
 {
     // MeshGenerator에 의해 생성된 Mesh를 MeshFillter및 MeshRenderer에 적용하는 역활을 합니다.
-    public class MeshGeneratorSetter : MonoBehaviour
+    public class HexagonalTest : MonoBehaviour
     {
         public MeshFilter meshFilter;
 
@@ -20,8 +20,8 @@ namespace BM.MeshGenerator
             var subdividiedIcosahedron = meshGenerator.SubdivideMesh(icosahedron);
             // TODO subdivideMesh에서 파라메터로 몇번 나눌 수 있게 할지 해야함
             subdividiedIcosahedron = meshGenerator.SubdivideMesh(subdividiedIcosahedron);
-            subdividiedIcosahedron.normalizeSphere(Vector3.zero);
-            subdividiedIcosahedron.RecalculateNormals();
+            //subdividiedIcosahedron.normalizeSphere(Vector3.zero);
+            //subdividiedIcosahedron.RecalculateNormals();
 
             var model = meshGenerator.MakeModel(subdividiedIcosahedron);
             var newMesh = meshGenerator.MakeMesh(model);
