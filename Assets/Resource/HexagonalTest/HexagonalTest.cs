07b6492b5b146e5443de9d2b2ab136407265041a
kbmhansungb <kbmhansungb@gmail.com>
2023-03-26 15:37:45 +0900

    구체 노말라이즈를 복구

--- Assets/Resource/HexagonalTest/HexagonalTest.cs
+++ Assets/Resource/HexagonalTest/HexagonalTest.cs
@@ -12,15 +12,12 @@ namespace BM.MeshGenerator
 
         void Start()
         {
-            if (meshFilter == null)
-                return;
-
             var meshGenerator = new MeshGenerator();
             var icosahedron = meshGenerator.CreateIcosahedron();
             var subdividiedIcosahedron = meshGenerator.SubdivideMesh(icosahedron);
             // TODO subdivideMesh에서 파라메터로 몇번 나눌 수 있게 할지 해야함
             subdividiedIcosahedron = meshGenerator.SubdivideMesh(subdividiedIcosahedron);
-            //subdividiedIcosahedron.normalizeSphere(Vector3.zero);
+            subdividiedIcosahedron.normalizeSphere(Vector3.zero);
             //subdividiedIcosahedron.RecalculateNormals();
 
             var model = meshGenerator.MakeModel(subdividiedIcosahedron);
